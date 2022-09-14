c020de899ba2d3926a258e53f6fda9cb90090f68
karim <hajji.karim.solicode@gmail.com>
2022-09-14 03:25:40 +0100

    firstcommit

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+// Begin script of sign_in 
+var your_div      = document.getElementById("your-div"); 
+var container     = document.getElementById("container");
+
+var result_width = (container.scrollWidth - your_div.scrollWidth) / 2;
+  var result_height = (container.scrollHeight - your_div.scrollHeight) /2;
+
+your_div.style.left = result_width + "px";
+your_div.style.top  = result_height + "px";
+
+
+function Myfunction()
+{
+  var result_width = (container.scrollWidth - your_div.scrollWidth) / 2;
+  var result_height = (container.scrollHeight - your_div.scrollHeight) /2;
+  your_div.style.left = result_width + "px";
+  your_div.style.top  = result_height + "px";
+  // alert("this width :"+ container.clientWidth +"this width :"+ your_div.clientWidth )
+  // alert("left :"+ result_width)
+}
+window.addEventListener('resize', Myfunction);
+// finish script of sgin_in
+
+
---
+++ JS/main_profile.js
@@ -0,0 +1,63 @@
+var valid = 1;
+var navbar = document.getElementById("navbar");
+var navbar_2 = document.getElementById("navbar_2");
+var your_div      = document.getElementsByClassName("level")[0]; 
+var container     = document.getElementsByClassName("container_level")[0];
+// Begin script of profile
+
+function burger()
+{
+            if(valid == 1)
+            {
+                navbar.style.left = "0px";
+                navbar.style.transition =   "0.8s";
+                valid = 0;
+            }
+            else
+            {
+                navbar.style.left = "-200px";
+                navbar.style.transition =   "0.8s";
+                valid = 1;
+            }
+}
+function burger_2()
+{
+            if(valid == 1)
+            {
+                navbar_2.style.left = "0px";
+                navbar_2.style.transition =   "0.8s";
+                valid = 0;
+            }
+            else
+            {
+                navbar_2.style.left = "-100%";
+                navbar_2.style.transition =   "0.8s";
+                valid = 1;
+            }
+}
+// finish
+
+// Begin resize container level
+
+
+var result_width  = (container.clientWidth - your_div.clientWidth) / 2;
+var result_height = (container.scrollHeight - your_div.scrollHeight) /2;
+
+your_div.style.left = result_width + "px";
+your_div.style.top  = result_height + "px";
+
+
+function Myfunction()
+{
+    var result_width = (container.clientWidth - your_div.clientWidth) / 2;
+    var result_height = (container.scrollHeight - your_div.scrollHeight) /2;
+    your_div.style.left = result_width + "px";
+    your_div.style.top  = result_height + "px";
+}
+
+window.addEventListener('resize', Myfunction);
+
+
+// finsh
+
+// 
