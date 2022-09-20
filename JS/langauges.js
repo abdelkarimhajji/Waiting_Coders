43535b2246614c5bc4bdce34b771c9f2f9aa478b
karim <hajji.karim.solicode@gmail.com>
2022-09-20 18:43:26 +0100

    add button in page languages

--- JS/langauges.js
+++ JS/langauges.js
@@ -1,16 +1,21 @@
-var Finished = document.getElementById("Finished");
+var Finished = document.getElementsByClassName("Finished").length
+var finished = document.getElementById("finished");
 var valid = 1;
-function changeColor()
-{
-    if (valid == 1)
+
+for(var i = 0; i < Finished; i++)
     {
-        Finished.setAttribute("class", "color_2");
-        valid = 0;
+        document.getElementsByClassName("Finished")[i].onclick = function()
+        {
+            
+            if(this.style.backgroundColor == "rgb(220, 53, 69)")
+            {
+                // this.setAttribute('style', 'backgroundColor : blue');
+                this.style.backgroundColor = "rgb(40, 167, 69)";
+            }
+            else
+            {
+                this.style.backgroundColor = "rgb(220, 53, 69)";
+            }
+            
+        }
     }
-    else
-    {
-        Finished.setAttribute("class", "color_1");
-        valid = 1;
-    }
-    
-}
