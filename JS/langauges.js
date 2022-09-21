ccac95d8993683cf23eae5cd0470b4e9602214ea
karim <hajji.karim.solicode@gmail.com>
2022-09-21 12:10:12 +0100

    update in the button ,page language

--- JS/langauges.js
+++ JS/langauges.js
@@ -1,21 +1,44 @@
 var Finished = document.getElementsByClassName("Finished").length
 var finished = document.getElementById("finished");
+var model_confirm = document.getElementById("model_confirm");
+var yes = document.getElementById("yes");
+var body = document.getElementsByTagName("body")[0];
+var test;
 var valid = 1;
-
+// update color of input finished
 for(var i = 0; i < Finished; i++)
     {
         document.getElementsByClassName("Finished")[i].onclick = function()
         {
-            
-            if(this.style.backgroundColor == "rgb(220, 53, 69)")
-            {
-                // this.setAttribute('style', 'backgroundColor : blue');
-                this.style.backgroundColor = "rgb(40, 167, 69)";
-            }
-            else
-            {
-                this.style.backgroundColor = "rgb(220, 53, 69)";
-            }
-            
+            model_confirm.style.display = "flex";
+            body.style.overflow = "hidden"
+            test = this;
         }
     }
+    // finish input finished
+
+// begin function close and cofirm
+function closeModel() {
+    model_confirm.style.display = "none";
+}
+
+function changeColor() {
+    closeModel();
+        if(test.style.backgroundColor == "rgb(220, 53, 69)")
+        {
+            test.style.backgroundColor = "rgb(40, 167, 69)";
+        }
+}
+
+function notChange(){
+    closeModel();
+    if(test.style.backgroundColor == "rgb(40, 167, 69)")
+        {
+            test.style.backgroundColor = "rgb(220, 53, 69)";
+        }
+}
+if(document.getElementById('button').clicked == true)
+{
+    alert("button was clicked");
+}
+// finish function close and confirm
